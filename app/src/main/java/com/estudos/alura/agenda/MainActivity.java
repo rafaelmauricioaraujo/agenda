0b13ece69e804ffc2b901a2728a44efcf2b48d19
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2019-09-22 15:40:43 -0300

    usando arquivo estatico de layout

--- app/src/main/java/com/estudos/alura/agenda/MainActivity.java
+++ app/src/main/java/com/estudos/alura/agenda/MainActivity.java
@@ -12,10 +12,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TextView aluno = new TextView(this);
-        aluno.setText("Rafael");
-        setContentView(aluno);
+        setContentView(R.layout.main_activity);
     }
 
 }
