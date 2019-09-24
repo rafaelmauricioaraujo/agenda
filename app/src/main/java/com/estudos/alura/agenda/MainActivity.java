e6957c2ed12d899e6350d4c1f17aa201e465cd33
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2019-09-23 21:59:33 -0300

    Configurando uma lista adaptativa

--- app/src/main/java/com/estudos/alura/agenda/MainActivity.java
+++ app/src/main/java/com/estudos/alura/agenda/MainActivity.java
@@ -2,10 +2,17 @@ package com.estudos.alura.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +20,12 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Rafael", "Mauricio", "Araujo"));
+
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+
     }
 
 }
